1766a77396b3d21255385380a50bfe701a127908
Alophox <bluebolt1@hotmail.com>
2026-01-11 15:34:36 -0700

    Capital Standoff fixes

--- 2-Units/Ships/7-Amethyst-Dreadnought/1011701 ARC Amethyst-BBBB.unit.lua
+++ 2-Units/Ships/7-Amethyst-Dreadnought/1011701 ARC Amethyst-BBBB.unit.lua
@@ -158,8 +158,8 @@ return {
 		reverseAccelMultiplier = functions.averageMultiplier({healthStats.retreatMult.B,healthStats.retreatMult.B,healthStats.retreatMult.B,healthStats.retreatMult.B}),      -- float: Fraction of accel used for reverse/breaking
 		inertialCorrection = true,         --Try to cancel out excess velocity in directions you don't want to go.
 		isStrafing = false,                 --Should this unit strafe around? (Partell, Skua)
-		standoffDistance = weaponStats.fireRangeMult * weaponStats.rangeMult["X"] * weaponStats.lightning.baseRange-5,              -- float: How far away from a target's ColliderDimensions should a unit hold position?
-		retreatDistance = weaponStats.fireRangeMult * weaponStats.rangeMult["L"] * weaponStats.lightning.baseRange,               -- float: How far away from a target's ColliderDimensions should a unit begin to retreat?
+		standoffDistance = weaponStats.fireRangeMult * weaponStats.rangeMult["L"] * weaponStats.cannon.baseRange-1,              -- float: How far away from a target's ColliderDimensions should a unit hold position?
+		retreatDistance = weaponStats.fireRangeMult * weaponStats.rangeMult["L"] * weaponStats.cannon.baseRange/2,               -- float: How far away from a target's ColliderDimensions should a unit begin to retreat?
 		strafeMargin = 1.5,                -- float: How far away from standoffDistance strafe thrust is allowed to start. Default = 1
 		useMinimumWorldYPosition = false,
 		minimumWorldYPosition = 0,         -- float: What does this unit consider the lowest it can go? Capitals and Heavies often use 3~4 to stop them sinking into bases, and keep them looking imposing.
