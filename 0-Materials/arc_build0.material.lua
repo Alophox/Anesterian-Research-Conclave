8de7e4609124f8250d2751c2776027ffe70640a7
Alophox <bluebolt1@hotmail.com>
2026-01-25 19:18:37 -0700

    Build mats
    Experimental; initially used for testing UV maps

---
+++ 0-Materials/arc_build0.material.lua
@@ -0,0 +1,33 @@
+return {
+    id = "arc_build0",
+    -- shader = "Shader Graphs/buildGlowShader",
+    -- keywords = { "_EMISSION" },
+    -- renderQueue = 3001,
+    -- properties = {
+    --     _Min = 1.3,
+    --     _QueueControl = 1,
+    --     _QueueOffset = -1,
+    --     Color_4e6c69b0723b433ba7b2ddf9d9e887ce = { 0.75, 1, 1, 10 },
+    --     Vector1_2edd504c70304e2f9eefa2291bb5775e = 0.5, --.5
+    --     Vector1_57479a3bbb204dd4b4d1285d181e9aff = 0.15, --.15
+    --     Vector1_b5d790f10c0541db948b46eaf179cc84 = 0.008, --.008
+    --     Vector1_f39efe0a5f1e4f228c03e1f79e29c522 = 10, --10
+    -- },
+
+    shader = "Shader Graphs/Ship Hull Shader",
+    properties = {
+        _CloakOpacity = 1,
+        _Color = { 0.75, 0.75, 0.75, 1 },
+        _DitherScale = 1,
+        _EmissionColor = { 0, 0, 0, 0 },
+        _Metallic = 0,
+        _QueueControl = 0,
+        _QueueOffset = 0,
+        _Smoothness = 0.5,
+        BaseSkuff = 0,
+        Damage = 0,
+        Heat = 0,
+    },
+
+
+}
---
+++ 0-Materials/arc_build4.material.lua
@@ -0,0 +1,18 @@
+return {
+    id = "arc_build4",
+    shader = "Shader Graphs/buildGlowShader",
+    keywords = { "_EMISSION" },
+    renderQueue = 3001,
+    properties = {
+        _Min = 1.3,
+        _QueueControl = 1,
+        _QueueOffset = -1,
+        Color_4e6c69b0723b433ba7b2ddf9d9e887ce = { 0.5, 1, 1, 1 },
+        Vector1_2edd504c70304e2f9eefa2291bb5775e = 0.2,
+        Vector1_57479a3bbb204dd4b4d1285d181e9aff = -0.2,
+        Vector1_b5d790f10c0541db948b46eaf179cc84 = 0.038,
+        Vector1_f39efe0a5f1e4f228c03e1f79e29c522 = 40,
+    },
+
+
+}
