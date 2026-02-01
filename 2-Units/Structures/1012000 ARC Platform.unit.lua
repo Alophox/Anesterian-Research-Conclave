3ccbe75bfac2d425d7051f7ee1259e44dc3aa5b4
Alophox <bluebolt1@hotmail.com>
2026-01-31 22:45:55 -0700

    Remove build from platforms
    Shows build when SAILing in, which is odd and bad

--- 2-Units/Structures/1012000 ARC Platform.unit.lua
+++ 2-Units/Structures/1012000 ARC Platform.unit.lua
@@ -85,6 +85,33 @@ return {
 			scale 	= { 1, 1, 1 },			--The nonuniform scale of the part, relative to it's parent's scale.
 			aegisVisual = true,
 		},
+		-- {
+		-- 	name     = "Build",
+		-- 	mesh    = "Core/Core",
+		-- 	materials = { "0_arc_build", "0_arc_build", "0_arc_build", "0_arc_build" },
+		-- 	pos       = { 0, 0, 0 },        --Relative local position of this object.
+		-- 	rotation  = { 0, 0, 0 },        	--Eular Angles XYZ, will apply rotation ZXY. Relative local rotation of this object.
+		-- 	scale 	= { 1, 1, 1 },			--The nonuniform scale of the part, relative to it's parent's scale.
+		-- 	destroyOnConstructionComplete = true,
+		-- },
+		-- {
+		-- 	name     = "Build",
+		-- 	mesh    = "Core/Core",
+		-- 	materials = { "1_arc_build", "1_arc_build", "1_arc_build", "1_arc_build" },
+		-- 	pos       = { 0, 0, 0 },        --Relative local position of this object.
+		-- 	rotation  = { 0, 0, 0 },        	--Eular Angles XYZ, will apply rotation ZXY. Relative local rotation of this object.
+		-- 	scale 	= { 1, 1, 1 },			--The nonuniform scale of the part, relative to it's parent's scale.
+		-- 	destroyOnConstructionComplete = true,
+		-- },
+		-- {
+		-- 	name     = "Build",
+		-- 	mesh    = "Core/Core",
+		-- 	materials = { "2_arc_build", "2_arc_build", "2_arc_build", "2_arc_build" },
+		-- 	pos       = { 0, 0, 0 },        --Relative local position of this object.
+		-- 	rotation  = { 0, 0, 0 },        	--Eular Angles XYZ, will apply rotation ZXY. Relative local rotation of this object.
+		-- 	scale 	= { 1, 1, 1 },			--The nonuniform scale of the part, relative to it's parent's scale.
+		-- 	destroyOnConstructionComplete = true,
+		-- },
 		{
 			name      = "Drone Bay N",
 			position  = { 0, 8*.1/scaleConst, .75*.1/scaleConst },
