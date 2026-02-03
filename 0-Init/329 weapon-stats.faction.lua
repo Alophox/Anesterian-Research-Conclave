26152d80614942de98e90a751ee860addcbfcfd8
Alophox <bluebolt1@hotmail.com>
2026-02-03 16:35:23 -0700

    missile range 12 -> 13
    i'm sure this won't break anything

--- 0-Init/329 weapon-stats.faction.lua
+++ 0-Init/329 weapon-stats.faction.lua
@@ -200,7 +200,7 @@ weaponStats.lightning = {
 
 -- missiles have two turrets per 'slot', so damage is half
 weaponStats.missile = {
-	baseRange	= 12,
+	baseRange	= 13,
 	baseRetargetRange = 11.5,
 	baseCloakRange = 2,
 	baseDMG		= 40,
