3100ff6b0423b45cfe722a7a37b71c3a17ff2fd9
Alophox <bluebolt1@hotmail.com>
2026-02-03 20:08:21 -0700

    fix build material, again, hopefully

--- 0-Materials/329_MT_arc_build2.material.lua
+++ 0-Materials/329_MT_arc_build2.material.lua
@@ -15,17 +15,15 @@ return {
     -- },
 
 
-    shader = "Shader Graphs/buildGlowShader",
+    shader = "Shader Graphs/shieldShader",
     keywords = { "_EMISSION" },
-    renderQueue = 3001,
     properties = {
-        _Min = 1.3,
-        _QueueControl = 10,
-        _QueueOffset = -1,
-        Color_4e6c69b0723b433ba7b2ddf9d9e887ce = { 0.75, 1, 1, 1 },
-        Vector1_2edd504c70304e2f9eefa2291bb5775e = 0.5, --.5
-        Vector1_57479a3bbb204dd4b4d1285d181e9aff = .9, --.15
-        Vector1_b5d790f10c0541db948b46eaf179cc84 = 0.008, --.008
-        Vector1_f39efe0a5f1e4f228c03e1f79e29c522 = 0, --10
+        _QueueControl = 0,
+        _QueueOffset = 0,
+        Color_4e6c69b0723b433ba7b2ddf9d9e887ce = { .75, 1, 1, 0 },
+        Vector1_57479a3bbb204dd4b4d1285d181e9aff = 0.005,
+        Vector1_625f6af73f0c44b193c89e9923953e4e = -0.5,
+        Vector1_bbd52e2809834c3faafdb261d1a58c6a = .7,
+        Vector1_f39efe0a5f1e4f228c03e1f79e29c522 = 100000,
     },
 }
