54ac66f01a337a7a29133e4ca76fc1862bdcb57f
Alophox <bluebolt1@hotmail.com>
2026-02-07 20:30:31 -0700

    Removed extra 0

--- 2-Units/Structures/Yards/3292014 Shipyard-4.unit.lua
+++ 2-Units/Structures/Yards/3292014 Shipyard-4.unit.lua
@@ -117,11 +117,11 @@ return {
 	-- 🟦 HEALTH & ARMOR
 	health = {
 		unitClass = "TITAN",       -- UNITCLASS: NONE, MISSILE, DRONE, LIGHT, MEDIUM, HEAVY, CAPITAL, TITAN
-		health = functions.floor((1 - healthStats.proportionRegenMax) * 325000),
+		health = functions.floor((1 - healthStats.proportionRegenMax) * 32500),
 		health_regen_per_second = healthStats.regen.C,
 		max_regen_frac = 0,
 		aegis_regen_per_second = healthStats.aegisRegen.C * healthStats.structAegisRegenMult,
-		aegisMaximum = functions.ceil(healthStats.proportionRegenMax * 325000),
+		aegisMaximum = functions.ceil(healthStats.proportionRegenMax * 32500),
 
 		armour = 10,                --Reduces incoming damage. Used to allow heavier ship classes to withstand many smaller opponents, but still being countered by anti-armour. Lights ~5, Mediums ~10, Heavies ~20, Capitals ~50
 		vulnerability_max = 0,   --Prevent the unit from losing more than X fraction of it's armour.
