e09feb468b0afd9a5fb79cf252cd2c022ba60aba
Alophox <bluebolt1@hotmail.com>
2026-02-15 14:57:44 -0700

    Titanic Missile changed to Light
    It has enough HP to be a light so why isn't it

--- 1-Weapons/0-Missile/3294005 Missile-5.unit.lua
+++ 1-Weapons/0-Missile/3294005 Missile-5.unit.lua
@@ -179,7 +179,7 @@ return {
 
 	-- 🟦 HEALTH & ARMOR
 	health = {
-		unitClass = "MISSILE",       -- string enum: UNITCLASS: NONE, MISSILE, DRONE, LIGHT, MEDIUM, HEAVY, CAPITAL, TITAN
+		unitClass = "LIGHT",       -- string enum: UNITCLASS: NONE, MISSILE, DRONE, LIGHT, MEDIUM, HEAVY, CAPITAL, TITAN
 		health = weaponStats.missile.baseHP * weaponStats.damageMult[weaponConst.size],              -- float: Health, also the unit's heat capacity.
 		health_regen_per_second = 0, -- float: Health regen per second. Duh.
 		max_regen_frac = 0,      -- float: The maximum health regen can regenerate back to. 0.2 == 20% of health. Health regen will stop when health hits this fraction of total health.
