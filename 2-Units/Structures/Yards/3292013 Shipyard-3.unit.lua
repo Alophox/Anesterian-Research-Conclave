4fb6aa79b11940d1dce4e74f4997d1c8cb28d52e
Alophox <bluebolt1@hotmail.com>
2026-04-21 18:14:29 -0700

    fix aegis/health regen for medium and larger shipyards

--- 2-Units/Structures/Yards/3292013 Shipyard-3.unit.lua
+++ 2-Units/Structures/Yards/3292013 Shipyard-3.unit.lua
@@ -123,9 +123,9 @@ return {
 	health = {
 		unitClass = "CAPITAL",       -- UNITCLASS: NONE, MISSILE, DRONE, LIGHT, MEDIUM, HEAVY, CAPITAL, TITAN
 		health = functions.floor((1 - healthStats.proportionRegenMax) * 10000),
-		health_regen_per_second = healthStats.regen.C,
+		health_regen_per_second = healthStats.regen[shipConst.class],
 		max_regen_frac = 0,
-		aegis_regen_per_second = healthStats.aegisRegen.M * healthStats.structAegisRegenMult,
+		aegis_regen_per_second = healthStats.aegisRegen[shipConst.class] * healthStats.structAegisRegenMult,
 		aegisMaximum = functions.ceil(healthStats.proportionRegenMax * 10000),
 
 		armour = 10,                --Reduces incoming damage. Used to allow heavier ship classes to withstand many smaller opponents, but still being countered by anti-armour. Lights ~5, Mediums ~10, Heavies ~20, Capitals ~50
