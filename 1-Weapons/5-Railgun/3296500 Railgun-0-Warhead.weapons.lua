d0ba1f6263e7bcc8d5a5b35d45ff5d41f1a88858
Alophox <bluebolt1@hotmail.com>
2026-08-27 01:00:28 -0700

    refactor degrees

--- 1-Weapons/5-Railgun/3296500 Railgun-0-Warhead.weapons.lua
+++ 1-Weapons/5-Railgun/3296500 Railgun-0-Warhead.weapons.lua
@@ -172,7 +172,7 @@ return {
 			minimumHealth = 0, 				--Don't target things with less Max Health than this.
 			minimumDistance = 0, 			--Don't target things that are closer than this.
 			maximumDistance = 1, 			--Important. Scan radius. Don't target things further than this. KEEP THIS NUMBER LOW, SERIOUS PERFORMANCE IMPACT. Light ~15, Medium ~20, Heavy ~25, Capital ~25
-			maximumAngleDegrees = 20626.480624709635, 				--Good for spinal weapons/missiles.
+			maximumAngleDegrees = 360, 				--Good for spinal weapons/missiles.
 			addedPreaimDistance = 1, 		--If you have no target in maximumDistance, you may try to target something this far beyond max distance. (use on turrets, not ships)
 			
 			-- Scoring can be negative. Will invert behaviour.
